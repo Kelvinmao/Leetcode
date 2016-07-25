8f5da09b1533df272bd45dc9323a01027abf2fbe
Kelvin <kelvinmao@git.com>
2016-07-25 22:06:42 +0800

    2016-07-24

---
+++ array/Leetcode118.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> val;
+        val.resize(numRows);
+        int i=0,j=0;
+        for(i=0;i<numRows;i++){
+            val[i].resize(i+1);
+            val[i][0]=1;
+            val[i][val[i].size()-1]=1;
+            for(j=1;j<val[i].size()-1;j++)
+                val[i][j]=val[i-1][j-1]+val[i-1][j];
+        }
+        return val;
+    }
+};
---
+++ array/Leetcode27.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int new_len=0,i=0,size=nums.size();
+        for(i=0;i<nums.size();i++){
+            if(nums[i]!=val){
+                nums[new_len++]=nums[i];
+            }
+        }
+        return new_len;
+    }
+};
