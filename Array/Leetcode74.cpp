a59f9dd1c6d487f30351a03abbf524f1d9661ab8
Kelvin <kelvinmao@git.com>
2016-08-05 22:00:52 +0800

    2016-08-05

---
+++ Array/Leetcode74.cpp
@@ -0,0 +1,43 @@
+/*solution1 
+Traverse the 2D matrix*/
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        if(matrix.empty()||matrix[0].empty())
+            return false;
+        int i=0,j=0;
+        int row=matrix.size(),col=matrix[0].size();
+        for(i=0;i<row;i++)
+            for(j=0;j<col;j++)
+                if(matrix[i][j]==target)
+                    return true;
+        return false;
+    }
+};
+
+/*solution2
+use binary-search in rows*/
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        if(matrix.empty()||matrix[0].empty())
+            return false;
+        int row=matrix.size(),i=0;
+        int low=0,size=matrix[0].size(),high=size-1,mid=0;
+        for(i=0;i<row;i++){
+            low=0;
+            high=size-1;
+            while(low<=high){
+                mid=low+(high-low)/2;
+                if(matrix[i][mid]==target)
+                    return true;
+                if(matrix[i][mid]<target)
+                    low=mid+1;
+                else
+                    high=mid-1;
+            }
+        }
+        return false;
+    }
+};
+
