83caea0dd94e2359dad98ad4c917fb1b3b3cdf2f
Kelvin <kelvinmao@git.com>
2016-08-09 22:05:47 +0800

    2016-08-09

---
+++ Array/Leetcode169.cpp
@@ -0,0 +1,53 @@
+/*solution1 O(n^2) 20ms*/
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        if(nums.size()<2)
+            return nums[0];
+        int i=0,j=0,size=nums.size(),count=0;
+        int half=size/2;;
+        for(i=0;i<size;i++){
+            count=0;/*外循环每进行一次count计数变量应该重新置0*/
+            /*遇到有重复元素的，我们仅以最后一个元素做参照，来对该元素出现次数进行统计*/
+            while(nums[i]==nums[i+1])
+                i++;
+            for(j=0;j<size;j++){
+                if(nums[j]==nums[i]){
+                    count++;
+                    if(count>half)
+                        return nums[i];
+                }
+            }
+        }
+        return -1;
+    }
+};
+
+/*solution2 sort 40ms*/
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        return nums[nums.size()/2];
+    }
+};
+
+/*solution3 use majority voting algorithm*/
+/*solution1 O(n^2) 20ms*/
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        if(nums.size()<2)
+            return nums[0];
+        int size=nums.size(),count=1,curIndex=0;
+        for(int i=1;i<size;i++){
+            nums[i]==nums[curIndex]?count++:count--;
+            if(!count){
+                curIndex=i;
+                count=1;
+            }
+        }
+        return nums[curIndex];
+    }
+};
+
