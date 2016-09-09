3250ac56635d5fdfa44915b77eb2607b3cda4f08
Kelvin <kelvinmao@git.com>
2016-09-09 21:44:04 +0800

    2016-09-09

--- LinkedList/Leetcode328.cpp
+++ LinkedList/Leetcode328.cpp
@@ -18,3 +18,28 @@ struct ListNode* oddEvenList(struct ListNode* head) {
     odd->next=even_head;
     return head;
 }
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if(!head||!head->next)
+            return head;
+        struct ListNode *odd=head,*even_head=head->next,*even=even_head;
+        while(even&&even->next){
+            odd->next=even->next;
+            odd=odd->next;
+            even->next=odd->next;
+            even=even->next;
+        }
+        odd->next=even_head;
+        return head;
+    }
+};
