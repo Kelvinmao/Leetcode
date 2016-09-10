f87654313d6063e33f33713bf5feeb08f7b92372
Kelvin <kelvinmao@git.com>
2016-09-10 15:05:15 +0800

    2016-09-10

--- LinkedList/Leetcode86.cpp
+++ LinkedList/Leetcode86.cpp
@@ -25,3 +25,35 @@ struct ListNode* partition(struct ListNode* head, int x) {
     pLeft->next=right.next;
     return left.next;
 }
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* partition(ListNode* head, int x) {
+        struct ListNode left(0),right(0);
+        struct ListNode *pLeft=&left,*pRight=&right;
+        if(!head)
+            return head;
+        while(head){
+            if(head->val<x){
+                pLeft->next=head;
+                pLeft=pLeft->next;
+            }
+            else{
+                pRight->next=head;
+                pRight=pRight->next;
+            }
+            head=head->next;
+        }
+        pRight->next=NULL;
+        pLeft->next=right.next;
+        return left.next;
+    }
+};
