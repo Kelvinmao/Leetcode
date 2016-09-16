9743f1c0e269a0e45ef7788fa27f8da7f6279ff7
Kelvin <kelvinmao@git.com>
2016-09-16 17:57:52 +0800

    2016-09-16

--- Array/Leetcode18.cpp
+++ Array/Leetcode18.cpp
@@ -1,4 +1,3 @@
-/*solution 1*/
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
@@ -10,27 +9,28 @@ public:
             return res;
         for(left=0;left<size-3;left++){
             int tmp_left=target-nums[left];
-            /*去除left造成的重复*/
-			if(left > 0 && nums[left] == nums[left-1])
+            if(left > 0 && nums[left] == nums[left-1])
+                continue;
+            if(nums[left]+nums[left+1]+nums[left+2]+nums[left+3]>target)
+                break;
+            if(nums[left]+nums[size-3]+nums[size-2]+nums[size-1]<target)
                 continue;
             for(mid1=left+1;mid1<size-2;mid1++){
                 int tmp=tmp_left-nums[mid1];
                 mid2=mid1+1;
                 right=size-1;
-                /*去除mid1造成的重复*/
-				if(mid1 > left+1 && nums[mid1] == nums[mid1-1])
+                if(mid1 > left+1 && nums[mid1] == nums[mid1-1])
+                    continue;
+                if(nums[left]+nums[mid1]+nums[mid1+1]+nums[mid1+2]>target)
+                    break;
+                if(nums[left]+nums[mid1]+nums[size-2]+nums[size-1]<target)
                     continue;
                 while(mid2<right){
                     if(nums[mid2]+nums[right]==tmp){
                         tmp_mid2=nums[mid2];
                         tmp_right=nums[right];
-                        vector<int>quadruplets(4,0);
-                        quadruplets[0]=nums[left];
-                        quadruplets[1]=nums[mid1];
-                        quadruplets[2]=nums[mid2];
-                        quadruplets[3]=nums[right];
-                        res.push_back(quadruplets);
-                        while(mid2 < right && nums[++mid2] == tmp_mid2);/*去除mid2造成的重复*/
+                        res.push_back({nums[left],nums[mid1],nums[mid2],nums[right]});
+                        while(mid2 < right && nums[++mid2] == tmp_mid2);
                         while(mid2 < right && nums[--right] == tmp_right);
                     }
                     else if(nums[mid2]+nums[right]<tmp)
