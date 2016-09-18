c5c7ca55a4f6acdaad3494e8707e43f4fd221a3a
Kelvin <kelvinmao@git.com>
2016-09-18 10:25:58 +0800

    2016-09-18

--- Hash-table/Leetcode347.cpp
+++ Hash-table/Leetcode347.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
-    vector<int> topKFrequent(vector<int>& nums, int k) {
-        unordered_map<int,int> hash;
-        vector<int> res;
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> heap;/*第一个参数指定元素类型，第二个参数决定使用哪一种容器，
+    vector<int> topKFrequent(vector<int>& nums, int k) {    
+        unordered_map<int,int> map;
+        priority_queue<pair<int,int>,vector<pair<int,int>>> heap;/*第一个参数指定元素类型，第二个参数决定使用哪一种容器，
 																						第三个用于建立小顶堆*/
+        vector<int> res;
         for(auto num:nums)
-            hash[num]++;
-        for(auto pos : hash){
+            map[num]++;
+        for(auto pos:map)
             heap.push({pos.second,pos.first});
-            if(heap.size()>k)
-                heap.pop();
-        }
-        while(!heap.empty()){
+        for(int i=0;i<k;i++){
             res.push_back(heap.top().second);
             heap.pop();
         }
